e74bc2c167b2f8f89be087ac8e71685560cf5bce
SandrajSaju <sandrajdevamangalam@gmail.com>
2023-08-01 23:34:23 +0530

    Login Added

--- test.js
+++ test.js
@@ -5,6 +5,10 @@ app.get("/",(req,res)=>{
     res.send("Hello GIT 3000!!!")
 })
 
+app.get("/login",(req,res)=>{
+    res.send("This is Login Page")
+})
+
 app.listen(3000,()=>{
     console.log("Listening on port 3000");
 })
