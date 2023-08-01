a6ebbd139a340565753d657959b3699557dc2375
SandrajSaju <sandrajdevamangalam@gmail.com>
2023-08-01 18:08:46 +0530

    Login Page on 4000

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express=require("express")
 const app=express()
 
-app.get("/",(req,res)=>{
-    res.send("Hello GIT 4000!!!")
-})
-
 app.get("/login",(req,res)=>{
     res.send("Login Page")
 })
+
 
 app.listen(4000,()=>{
     console.log("Listening on port 4000");
