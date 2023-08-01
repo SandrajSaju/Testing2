20476c41511f55ba40f9e18954c224d293a3663e
SandrajSaju <sandrajdevamangalam@gmail.com>
2023-08-01 23:43:51 +0530

    Admin Added

--- test.js
+++ test.js
@@ -4,6 +4,9 @@ const app=express()
 app.get("/",(req,res)=>{
     res.send("Hello GIT 3000!!!")
 })
+app.get("/admin",(req,res)=>{
+    res.send("Hello Admin Added")
+})
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000");
